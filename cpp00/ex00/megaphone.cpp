1c711a3720bff9b1e18b04719884ae717f753987
Teresa Bulungu Lufuluabo Pastor <telufulu@c1r7s3.42madrid.com>
2025-08-23 20:47:51 +0200

    ex00 OK

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,11 +6,11 @@
 /*   By: telufulu <telufulu@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/21 16:21:54 by telufulu          #+#    #+#             */
-/*   Updated: 2025/08/21 16:51:33 by telufulu         ###   ########.fr       */
+/*   Updated: 2025/08/23 20:47:10 by telufulu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>	// cout
+#include <iostream>	// cout, endl
 #include <cctype>	// toupper
 
 int	main(int argc, char **argv)
@@ -18,17 +18,15 @@ int	main(int argc, char **argv)
 	if (argc == 1)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return(0);
+		return (0);
 	}
+	++argv;
 	while (argv && *argv)
 	{
 		while (*argv && **argv)
-		{
-			std::cout << toupper(**argv);
-			(*argv)++;
-		}
-		std::cout << std::endl;
+			std::cout << char(toupper(*(*argv)++));
 		++argv;
 	}
+	std::cout << std::endl;
 	return (0);
 }
