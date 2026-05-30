5b2c1ac1f2431118231fede333cb206e1acd62b0
telufulu <teresa@teresalufuluabo.es>
2026-05-30 23:19:43 +0200

    to char is OK

--- cpp06/ex00/srcs/ScalarConverter.cpp
+++ cpp06/ex00/srcs/ScalarConverter.cpp
@@ -1,11 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: telufulu <@student.42madrid.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/30 22:55:19 by telufulu          #+#    #+#             */
+/*   Updated: 2026/05/30 22:55:21 by telufulu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>		// cout
+#include <cstdlib>		// strtod
 #include "ScalarConverter.hpp"
 
 /* ****************************************************************************	*/
 /*								CONSTRUCTORS									*/
 /* ****************************************************************************	*/
 
-ScalarConverter::ScalarConverter ( void ) : _data(0)
+ScalarConverter::ScalarConverter ( void )
 {
 	//std::cout << "\033[90mDefault ScalarConverter constructor called\033[0m" << std::endl;
 	return ;
@@ -14,7 +27,7 @@ ScalarConverter::ScalarConverter ( void ) : _data(0)
 ScalarConverter::ScalarConverter ( const ScalarConverter &cpy)
 {
 	//std::cout << "\033[90mCopy ScalarConverter constructor called\033[0m" << std::endl;
-	this->_data = cpy._data;
+	(void)cpy;
 	return ;
 }
 
@@ -27,14 +40,12 @@ ScalarConverter	&ScalarConverter::operator=( const ScalarConverter  &rhs )
 	//std::cout << "\033[90mCopy ScalarConverter operator called\033[0m" << std::endl;
 	if (this == &rhs)
 		return *this;
-	this->_data = rhs._data;
 	return *this;
 }
 
 /* ****************************************************************************	*/
 /*								DESTRUCTOR										*/
 /* ****************************************************************************	*/
-
 ScalarConverter::~ScalarConverter ( void )
 {
 	//std::cout << "\033[90mScalarConverter destructor called\033[0m" << std::endl;
@@ -42,8 +53,27 @@ ScalarConverter::~ScalarConverter ( void )
 }
 
 /* ****************************************************************************	*/
-/*							MEMBER FUNCTIONS									*/
+/*								MEMBER FUNCTIONS								*/
 /* ****************************************************************************	*/
+void	toChar( const std::string &literal, const double value )
+{
+	std::cout << "char : ";
+	if (literal.empty() || !std::isdigit(literal[0]))
+		std::cout << "imposible" << std::endl;
+	else if (value < 32 || value > 126)
+		std::cout << "non displayable" << std::endl;
+	else
+		std::cout << "'" << static_cast<char>(value) << "'" << std::endl;
+}
+
+void	ScalarConverter::convert( const std::string &literal )
+{
+	char	*end;
+	double	value;
+
+	value = std::strtod(literal.c_str(), &end);
+	toChar(literal, value);
+}
 
 /* ****************************************************************************	*/
 /*							NON MEMBER FUNCTIONS								*/
--- cpp06/ex00/srcs/main.cpp
+++ cpp06/ex00/srcs/main.cpp
@@ -10,9 +10,30 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+// Output example
+// ./convert 0
+// char: Non displayable
+// int: 0
+// float: 0.0f
+// double: 0.0
+
+// ./convert nan
+// char: impossible
+// int: impossible
+// float: nanf
+// double: nan
+
+// ./convert 42.0f
+// char: '*'
+// int: 42
+// float: 42.0f
+// double: 42.0
+
 #include "ScalarConverter.hpp"
 
 int	main ( int argc, char **argv )
 {
+	if (argc == 2)
+		ScalarConverter::convert(argv[1]);
 	return 0;
 }
