6f442a7d11220b56ccbd6ba21b37a807bfeea7ba
Teresa Sherpa <teresa@sherpa-advisory.com>
2026-06-26 21:13:32 +0200

    working...

--- cpp08/ex00/srcs/main.cpp
+++ cpp08/ex00/srcs/main.cpp
@@ -13,20 +13,44 @@
 #include "easyfind.hpp"
 #include <vector>
 #include <iostream>
+#include <limits>
+#include <cstdlib>		// atoi
+
 
 int	main ( int argc, char **argv )
 {
-	std::vector<int> v;
-	(void)argc;
+	std::vector<int>	v;
+	long				n;
 
 	v.push_back(42);
+	v.push_back(21);
+	v.push_back(16);
+	v.push_back(-1);
+	v.push_back(+5);
+	v.push_back(std::numeric_limits<int>::min());
+	v.push_back(std::numeric_limits<int>::max());
+	if (argc == 1)
+	{
+		std::cout << "Not enough arguments."
+		<< " Usage: ./easyfind <number>" << std::endl;
+		return 1;
+	}
+	n = std::atol(argv[1]);
+	if (n > std::numeric_limits<int>::max()
+		|| n < std::numeric_limits<int>::min())
+	{
+		std::cout << "Number out of int limits" << std::endl;
+		return 1;
+	}
 	try
 	{
-		easyfind(v, static_cast<int>(*argv[1]));
+		std::cout << "TEST 1: std::vector" << std::endl; 
+		::easyfind(v, std::atoi(argv[1]));
 		std::cout << "Finded!" << std::endl;
 	} catch (...)
 	{
 		std::cout << "Not found" << std::endl;
 	};
+	//std::cout << std::endl;
 	return 0;
 }
